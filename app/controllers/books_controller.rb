b390932b79d85736e6e362fe2817f9ef083ea014
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2023-02-23 17:46:15 +0900

    prepare about database stuffs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,8 +29,31 @@ class BooksController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html {  redirect_to @book, notice: "Book was successfully updated" }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
-    
+  end
+
+  def edit
+  end
+
+  def destroy
+    @book.destroy
+
+    respond_to do |format|
+      format.html { redirect_to books_url }
+      format.json { head :no_content }
+    end
   end
 
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :reviews
+  resources :fan_comments
+  resources :authors
+  resources :users
   # root 'books#index'
   resources :books
 end
